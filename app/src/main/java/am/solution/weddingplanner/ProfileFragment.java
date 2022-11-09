7a3275e2caa723774d6343499d19b9e933ef95a8
ancaavram22 <80705591+ancaavram22@users.noreply.github.com>
2022-11-09 21:25:27 +0200

    guests page and account page design

--- app/src/main/java/am/solution/weddingplanner/ProfileFragment.java
+++ app/src/main/java/am/solution/weddingplanner/ProfileFragment.java
@@ -1,12 +1,11 @@
 package am.solution.weddingplanner;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
 
@@ -15,5 +14,6 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+
     }
 }
--- app/src/main/java/am/solution/weddingplanner/bottomSheetFragment/CreateTaskBottomSheetFragment.java
+++ app/src/main/java/am/solution/weddingplanner/bottomSheetFragment/CreateTaskBottomSheetFragment.java
@@ -1,39 +1,13 @@
 package am.solution.weddingplanner.bottomSheetFragment;
 
-import static android.content.Context.ALARM_SERVICE;
-
-import android.annotation.SuppressLint;
-import android.app.AlarmManager;
-import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.PendingIntent;
-import android.app.TimePickerDialog;
-import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-
-import am.solution.weddingplanner.R;
-import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
-import am.solution.weddingplanner.TasksFragment;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
+import am.solution.weddingplanner.R;
 
 public class CreateTaskBottomSheetFragment extends BottomSheetDialogFragment {
 

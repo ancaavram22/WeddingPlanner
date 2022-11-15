13485e3b70a45ee4a35b6eed6a140c9345a996c5
mirunaborcan <miruna.borcan@student.upt.ro>
2022-11-15 23:30:53 +0200

    Task database + create-new-task interface linked with backlog

--- app/src/main/java/am/solution/weddingplanner/bottomSheetFragment/CreateTaskBottomSheetFragment.java
+++ app/src/main/java/am/solution/weddingplanner/bottomSheetFragment/CreateTaskBottomSheetFragment.java
@@ -23,24 +23,57 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 
+import am.solution.weddingplanner.MainActivity;
 import am.solution.weddingplanner.R;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
+import androidx.fragment.app.Fragment;
+
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+import am.solution.weddingplanner.RegisterActivity;
 import am.solution.weddingplanner.TasksFragment;
+import am.solution.weddingplanner.model.User;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class CreateTaskBottomSheetFragment extends BottomSheetDialogFragment {
-
+    EditText addTaskTitle;
+    EditText addTaskDescription;
+    EditText addTaskDate;
+    EditText addTaskTime;
+    EditText addTaskEvent;
+    Button createTaskbutton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_task, container, false);
+        View view = inflater.inflate(R.layout.fragment_create_task, container, false);
+        addTaskTitle = view.findViewById(R.id.addTaskTitle);
+        addTaskDescription = view.findViewById(R.id.addTaskDescription);
+        addTaskDate = view.findViewById(R.id.taskDate);
+        addTaskTime = view.findViewById(R.id.taskTime);
+        addTaskEvent = view.findViewById(R.id.taskEvent);
+
+        createTaskbutton = view.findViewById(R.id.createTask);
+        createTaskbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String taskTitle = addTaskTitle.getText().toString().trim();
+                String taskDescription = addTaskDescription.getText().toString().trim();
+                String taskDate = addTaskDate.getText().toString().trim();
+                String taskTime = addTaskTime.getText().toString().trim();
+                String taskEvent = addTaskEvent.getText().toString().trim();
+//                System.out.println("verify if data is extracted" + taskTitle);
+//                System.out.println("verify if data is extracted" + taskDescription);
+//                System.out.println("verify if data is extracted" + taskDate);
+//                System.out.println("verify if data is extracted" + taskTime);
+            }
+        });
+        return view;
     }
+
 }
---
+++ app/src/main/java/am/solution/weddingplanner/data/TaskDAO.java
@@ -0,0 +1,23 @@
+package am.solution.weddingplanner.data;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import am.solution.weddingplanner.model.Task;
+
+@Dao
+public interface TaskDAO {
+    @Query("SELECT * FROM Task where  taskUser= :username ")
+    Task getUser( String username);
+
+    @Insert
+    void insert(Task task);
+
+    @Update
+    void update(Task task);
+
+    @Delete
+    void delete(Task task);
+}

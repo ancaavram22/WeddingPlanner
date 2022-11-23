0a720dabc58ef5bbc9c8a03779521b311a16321a
ancaavram22 <80705591+ancaavram22@users.noreply.github.com>
2022-11-23 21:19:36 +0200

    new activities for changing passwords, ui only

--- app/src/main/java/am/solution/weddingplanner/ProfileFragment.java
+++ app/src/main/java/am/solution/weddingplanner/ProfileFragment.java
@@ -1,19 +1,34 @@
 package am.solution.weddingplanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
+    TextView changePassword;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
 
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+       changePassword = view.findViewById(R.id.changePass);
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ChangePasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
+
+
 }
